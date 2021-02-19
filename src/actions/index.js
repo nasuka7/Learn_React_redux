db5816493044242e4314a7d2ca6ee7607d4a7a39
nasuka7 <n4suka7@gmail.com>
2021-02-19 23:54:33 +0900

    events new

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,10 +3,10 @@ import axios from "axios"
 export const READ_EVENTS = "READ_EVENTS"
 
 const ROOT_URL = "https://udemy-utils.herokuapp.com/api/v1"
-const QUARYSTRING = "?token=token123"
+const QUERYSTRING = "?token=token123"
 
 export const readEvents = () => async dispatch => {
-    const response = await axios.get(`${ROOT_URL}/events${QUARYSTRING}`)
+    const response = await axios.get(`${ROOT_URL}/events${QUERYSTRING}`)
     dispatch({ type: READ_EVENTS, response })
 }
 
