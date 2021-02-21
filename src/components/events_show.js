08cc043cdda61ceddbaba97881fa2cd8fba069ba
nasuka7 <n4suka7@gmail.com>
2021-02-21 22:52:53 +0900

    implement delete feature

--- src/components/events_show.js
+++ src/components/events_show.js
@@ -3,12 +3,13 @@ import { connect } from "react-redux"
 import { Field, reduxForm } from "redux-form"
 import { Link } from "react-router-dom"
 
-import { postEvent } from "../actions"
+import { getEvent, deleteEvent, putEvent } from "../actions"
 
-class EventsNew extends Component {
+class EventsShow extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.onSubmit = this.onSubmit.bind(this)
+    this.onDeleteClick = this.onDeleteClick.bind(this)
   }
   renderField(field) {
     const { input, label, type, meta: { touched, error }} = field
@@ -21,8 +22,15 @@ class EventsNew extends Component {
     )
   }
 
+  async onDeleteClick() {
+    const {id} = this.props.match.params
+    await this.props.deleteEvent(id)
+    this.props.history.push("/")
+
+  }
+
   async onSubmit(values) {
-    await this.props.postEvent(values)
+    // await this.props.postEvent(values)
     this.props.history.push("/")
   }
   
@@ -41,6 +49,7 @@ class EventsNew extends Component {
         <div>
           <input type="submit" value="Submit" disabled={pristine || submitting} />
           <Link to="/">Cancel</Link>
+          <Link to="/" onClick={this.onDeleteClick} >Delete</Link>
         </div>
       </form>
     )
@@ -56,8 +65,8 @@ const validate = values => {
   return errors
 }
 
-const  mapDispatchToProps= ({ postEvent })
+const  mapDispatchToProps= ({ deleteEvent })
 
 export default connect(null, mapDispatchToProps)(
-  reduxForm({ validate, form: 'eventNewForm' })(EventsNew)
+  reduxForm({ validate, form: 'eventShowForm' })(EventsShow)
 )
--- src/index.js
+++ src/index.js
@@ -4,22 +4,28 @@ import { createStore, applyMiddleware } from "redux"
 import { Provider } from "react-redux"
 import thunk from "redux-thunk"
 import { BrowserRouter, Route, Switch } from "react-router-dom"
+import { composeWithDevTools } from "redux-devtools-extension"
 
 import './index.css';
 import reducer from "./reducers"
 import EventsIndex from './components/events_index';
 import EventsNew from './components/events_new';
-import reportWebVitals from './reportWebVitals';
-// import registerServiceWorker from "./registerServiceWorker";
+import EventsShow from './components/events_show';
+// import reportWebVitals from './reportWebVitals';
+import registerServiceWorker from "./registerServiceWorker";
 
-const store = createStore(reducer, applyMiddleware(thunk))
+const enhancer = process.env.NODE_ENV === "development" ? 
+ composeWithDevTools(applyMiddleware(thunk)) : applyMiddleware(thunk)
+const store = createStore(reducer, enhancer)
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
       <Switch>
-        <Route exact path="/events/new" component={EventsNew} />
+        <Route path="/events/new" component={EventsNew} />
+        <Route path="/events:id" component={EventsShow} />
         <Route exact path="/" component={EventsIndex} />
+        <Route exact path="/events" component={EventsIndex} />
       </Switch>
     </BrowserRouter>
   </Provider>,
@@ -29,5 +35,5 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
-// registerServiceWorker();
+// reportWebVitals();
+registerServiceWorker();
